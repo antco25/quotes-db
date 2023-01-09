7ae03aee775ecd6bff0757d2993f5517dcf77a49
Anthony <anthonyco@hotmail.com>
2023-01-08 21:05:43 -0500

    initial

---
+++ src/main/java/com/antco/quotesdb/QuotesDbApplication.java
@@ -0,0 +1,13 @@
+package com.antco.quotesdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antco/quotesdb/QuotesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antco.quotesdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
